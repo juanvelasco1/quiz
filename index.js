86c2480a2601beabfce0492f07659871aab2ddb3
juanvelasco1 <juanesvb1@gmail.com>
2023-09-05 07:30:40 -0500

    1

---
+++ index.js
@@ -0,0 +1,10 @@
+import anyName from './data.js'
+console.log (anyName)
+
+
+function render(content) {
+    const  = document.createElementById('./data.js');
+    appElement.innerHTML = content;
+  }
+  
+anyName()
